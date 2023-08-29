99bd4fe72d19a16395e91bb040daf6c51c7e6918
resheidii <142684403+resheidii@users.noreply.github.com>
2023-08-29 19:49:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # write your code here
+
+favourite_animals = ["dog" , "penguins" , "rabbit" , "panda"]
+
+print (favourite_animals)
+
+print (favourite_animals [1])
+
+favourite_animals.remove ('rabbit')
+print (favourite_animals)
+
+favourite_animals.append ('horse')
+print (favourite_animals)
+
+for favourite_animals in favourite_animals :
+    print ( "i love " , favourite_animals ) 
+
+numbers = [ 1 , 2 , 3 , 4 , 5 ] 
+
+numbers_sum = 0
+
+for num in numbers : 
+
+    numbers_sum += num 
+    print (numbers_sum)
+
+
